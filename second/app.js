864f5a0d26f5ff58f5239de5880d50d80a776815
AAKASH HANDA <AAKASH HANDA>
2018-04-22 01:15:13 +0530

    first file

---
+++ second/app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+
+var app = express();
+
+var port = 4500;
+
+app.get("/",function(req,res){
+    res.send("this is home pages")
+})
+
+app.get("/books",function(req,res){
+    res.send("this is books page for my app lkdmkvlmkfm")
+})
+
+app.listen(port,function(err){
+    console.log(" server is running on port"+ port)
+})
+
+
+
+
+/*
+function add(a,b){
+    return a+b
+}
+
+npm install -g nodemon
+*/
